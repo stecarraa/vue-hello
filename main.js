c18815bb77b6c14b492bfd456d6b9790e3526680
stecarraa <stefano@caratozzolo.me>
2022-07-04 15:55:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un elemento h1, utilizzando i data nell'istanza di Vue.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa dai data.
+
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Super mega titolone alla Petricc!'
+    }
+  })
